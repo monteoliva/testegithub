9a2f7a75ff825baff5bca3173717cc513335380f
Luis Claudio Monteoliva <t.claudio.monteoliva@estapar.com.br>
2020-08-21 17:40:48 -0300

    updated

--- app/src/main/java/br/com/monteoliva/testegithub/view/main/MainActivity.kt
+++ app/src/main/java/br/com/monteoliva/testegithub/view/main/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 
 import kotlinx.android.synthetic.main.activity_main.*
